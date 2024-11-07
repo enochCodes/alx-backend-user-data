ff6cfb95548d7a8fe2250c6611dc3d665cbd57c6
Henok Samson <henoksamson57@gmail.com>
2024-11-07 23:36:58 +0300

    add  get db connection

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,9 @@ return logger object with filter."""
 import logging
 import re
 from typing import List
+import os
+import mysql.connector
+from mysql.connector.connection import MySQLConnection
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -60,3 +63,19 @@ def get_logger() -> logging.Logger:
     logger.addHandler(handler)
 
     return logger
+
+def get_db() -> MySQLConnection:
+    """Establishes and returns a connection to the database."""
+    # Retrieve environment variables with default values
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    # Connect to the database and return the connection object
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
