536c83949bed9af9fb1e0974b96f3368626eaa6f
Henok Samson <henoksamson57@gmail.com>
2024-11-07 22:27:50 +0300

    feat: add filter_datum function to obfuscate sensitive fields in log messages
    - Implement filter_datum function to replace values of specified fields in a log message with a redaction string.
    - Use regex pattern with re.sub to match and obfuscate field values dynamically based on the fields list.
    - Ensure the function is flexible with custom fields, redaction string, and separator character.
    - Function is designed to be efficient and concise, under 5 lines.
    
    Example use case: obfuscates fields like password and date_of_birth in log messages, enhancing data privacy.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,13 +6,10 @@ import re
 from typing import List
 
 
-def filter_datum(
-    fileds: List[str],
-    redaction: str,
-    message: str,
-    separator: str
-) -> str:
-    """Filter a datum."""
-    pattern = r'(' + '|'.join(
-        map(re.escape, fileds)) + ')=(.*?)(?=' + separator + ')'
-    return re.sub(pattern, r'\1=' + redaction, message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
