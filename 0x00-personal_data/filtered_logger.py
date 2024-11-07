f992981d5859608772548515f1d8e194db83c07b
Henok Samson <henoksamson57@gmail.com>
2024-11-07 23:44:51 +0300

    add

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,37 +19,6 @@ def filter_datum(fields: List[str], redaction: str,
     return message
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str] = []):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """ Format the log record """
-        return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
-
-    @staticmethod
-    def filter_datum(
-        fields: List[str],
-        redaction: str,
-        message: str,
-        separator: str
-    ) -> str:
-        """Obfuscate the values of specified fields in a log message."""
-        for f in fields:
-            message = re.sub(
-                f'{f}=.*?{separator}', f'{f}={redaction}{separator}', message)
-            return message
-
-
 def get_logger() -> logging.Logger:
     """ Returns a configured logger with
    specific settings. """
@@ -96,3 +65,25 @@ def main():
 
     cursor.close()
     db.close()
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str] = []):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the log record """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+if __name__ == '__main__':
+    main()
